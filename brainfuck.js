280139e5581285a2927b4aeb986bcf5ebffb0029
Grzegorz Kućmierz <gkucmierz@gmail.com>
2016-11-16 10:39:55 +0100

    more es6 and simplify code

--- brainfuck.js
+++ brainfuck.js
@@ -13,7 +13,7 @@ let config = {
   maxInstructions: 0 // limit execution to number of instructions, omit if 0
 };
 
-function getInstruction (count, orderLess, orderMore) {
+function getInstruction(count, orderLess, orderMore) {
   return ({
     '1': (count > 1) ? count + orderMore : orderMore,
     '0': '',
@@ -21,111 +21,95 @@ function getInstruction (count, orderLess, orderMore) {
   })[Math.sign(count)];
 }
 
-function escapeRegExp (str) {
+function escapeRegExp(str) {
   return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
 }
 
-function cloneObj (obj) {
+function cloneObj(obj) {
   return Object.keys(obj).reduce((res, key) => (res[key] = obj[key], res), {});
 }
 
-function extendObj (obj, ext) {
+function extendObj(obj, ext) {
   return Object.keys(ext || {}).reduce((res, key) => (res[key] = ext[key], res), obj);
 }
 
-module.exports.config = function (userConfig) {
+module.exports.config = (userConfig) => {
   if (typeof userConfig === 'undefined') {
     return cloneObj(config);
   }
   extendObj(config, userConfig);
 };
 
-module.exports.compile = function (bfSource, userConfig) {
+module.exports.compile = (bfSource, userConfig) => {
   let actualConfig = extendObj(cloneObj(config), userConfig);
   let cleanedSource = (bfSource+'').replace(regex.clean, '');
   let optimized = cleanedSource
-    .replace(regex.value, function (m) {
+    .replace(regex.value, (m) => {
       let map = { '+': 1, '-': -1 };
-      let n = m.split('').reduce(function (acc, b) {
-        return acc + map[b];
-      }, 0);
+      let n = m.split('').reduce((acc, b) => acc + map[b], 0);
       return getInstruction(n, '-', '+');
     })
-    .replace(regex.pointer, function (m) {
+    .replace(regex.pointer, (m) => {
       let map = { '>': 1, '<': -1 };
-      let n = m.split('').reduce(function (acc, b) {
-        return acc + map[b];
-      }, 0);
+      let n = m.split('').reduce((acc, b) => acc + map[b], 0);
       return getInstruction(n, '<', '>');
-    })
-  ;
+    });
   
   let ordersMap = { // m,p,o,i,l
-    ',': function () { return 'm[p]=i();'; },
-    '.': function () { return 'o(m[p]);'; },
-    '[': function () { return 'while(m[p]){'; },
-    ']': function () { return '}'; },
-    '<': function (count) { return 'p-='+count+';while(p<0)p+=l;'; },
-    '>': function (count) { return 'p+='+count+';while(p>=l)p-=l;'; },
-    '+': function (count) { return 'm[p]+='+count+';'; },
-    '-': function (count) { return 'm[p]-='+count+';'; }
+    ',': () => 'm[p]=i();',
+    '.': () => 'o(m[p]);',
+    '[': () => 'while(m[p]){',
+    ']': () => '}',
+    '<': (count) => 'p-='+count+';while(p<0)p+=l;',
+    '>': (count) => 'p+='+count+';while(p>=l)p-=l;',
+    '+': (count) => 'm[p]+='+count+';',
+    '-': (count) => 'm[p]-='+count+';'
   };
-  let createOrder = function (order, count) {
-    let prefix = actualConfig.maxInstructions > 0 ? [
-      'if(!--c)return;'
-    ].join('') : '';
+
+  let createOrder = (order, count) => {
+    // if there is a instruction limit, add prefix check-instruction to every instruction
+    let prefix = actualConfig.maxInstructions > 0 ? 'if(!--c)return;' : '';
     return [prefix, ordersMap[order](count)].join('');
   };
+
   let definitions = {
     // count
-    c: function (config) {
-      return config.maxInstructions > 0 ? 'let c='+config.maxInstructions+';' : '';
-    },
+    c: (config) => config.maxInstructions > 0 ? 'let c='+config.maxInstructions+';' : '',
     // length
-    l: function (config) {
-      return ['let l=',config.memorySize,';'].join('');
-    },
+    l: (config) => ['let l=', config.memorySize, ';'].join(''),
     // memory
-    m: function (config) {
-      let memConstructors = {
-        '8': 'Uint8Array',
-        '16': 'Uint16Array',
-        '32': 'Uint32Array'
-      };
-      let constructorName = memConstructors[config.bits] || memConstructors[8];
-      return ['let m=new ',constructorName,'(l);'].join('');
+    m: (config) => {
+      const constr = {'8': 'Uint8Array', '16': 'Uint16Array', '32': 'Uint32Array'};
+      return ['let m=new ', constr[config.bits] || constr[8], '(l);'].join('');
     },
     // pointer
-    p: function () {
-      return 'let p=0;';
-    },
+    p: () => 'let p=0;',
     // out
-    o: function () {
-      return 'let o=output||function(){};';
-    },
+    o: () => 'let o=output||(()=>0);',
     // in
-    i: function () {
-      return 'let i=input||function(){return 0;};';
-    }
+    i: () => 'let i=input||(()=>0);'
   };
+
   let args = ['input', 'output'];
 
+  // create variables definitions
   let code = Object.keys(definitions).map(key => definitions[key](actualConfig));
 
+  // create rest code
   (optimized.match(regex.instruction) || []).map((instruction) => {
-    let count = +instruction.substr(0, instruction.length - 1) || 1;
-    let order = instruction.substr(-1);
+    let count = +instruction.slice(0, -1) || 1;
+    let order = instruction.slice(-1);
     code.push(createOrder(order, count));
   });
 
   let compiled = new Function(args, code.join(''));
   return {
-    run: function (input, output) {
+    run: (input, output) => {
       let inp, out;
       let res = [];
       if (typeof input === 'string') {
         input = input.split('');
-        inp = function () {
+        inp = () => {
           let ch = input.shift();
           return ch ? ch.charCodeAt(0) : 0;
         };
@@ -133,9 +117,9 @@ module.exports.compile = function (bfSource, userConfig) {
         inp = input;
       }
       if (typeof output !== 'function') {
-        output = function () {};
+        output = () => 0;
       }
-      out = function (num) {
+      out = (num) => {
         let ch = String.fromCharCode(num);
         output(num, ch);
         res.push(ch);
@@ -143,8 +127,6 @@ module.exports.compile = function (bfSource, userConfig) {
       compiled(inp, out);
       return res.join('');
     },
-    toString: function () {
-      return compiled.toString();
-    }
+    toString: () => compiled.toString()
   };
 };
