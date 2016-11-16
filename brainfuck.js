55b63602da4571d2534395d968cc1c5ea8d3c0e5
Grzegorz Kućmierz <gkucmierz@gmail.com>
2016-11-16 09:42:05 +0100

    var -> let

--- brainfuck.js
+++ brainfuck.js
@@ -1,15 +1,15 @@
 
-var _ = require('lodash-node');
+let _ = require('lodash-node');
 
-var orders = ',.[]<>+-'.split('');
-var regex = {
+let orders = ',.[]<>+-'.split('');
+let regex = {
   clean: new RegExp('[^' + escapeRegExp(orders.join('')) + ']', 'g'),
   value: /[\+\-]+/g,
   pointer: /[\<\>]+/g,
   instruction: /[0-9]*./g
 };
 
-var config = {
+let config = {
   memorySize: 30000,
   bits: 8, // 8, 16, 32
   maxInstructions: 0 // limit execution to number of instructions, omit if 0
@@ -22,7 +22,7 @@ module.exports.config = function (userConfig) {
   _.extend(config, userConfig);
 };
 
-var getInstruction = function (count, orderLess, orderMore) {
+let getInstruction = function (count, orderLess, orderMore) {
   return ({
     '1': (count > 1) ? count + orderMore : orderMore,
     '0': '',
@@ -31,26 +31,26 @@ var getInstruction = function (count, orderLess, orderMore) {
 };
 
 module.exports.compile = function (bfSource, userConfig) {
-  var actualConfig = _.extend(_.clone(config), userConfig);
-  var cleanedSource = (bfSource+'').replace(regex.clean, '');
-  var optimized = cleanedSource
+  let actualConfig = _.extend(_.clone(config), userConfig);
+  let cleanedSource = (bfSource+'').replace(regex.clean, '');
+  let optimized = cleanedSource
     .replace(regex.value, function (m) {
-      var map = { '+': 1, '-': -1 };
-      var n = m.split('').reduce(function (acc, b) {
+      let map = { '+': 1, '-': -1 };
+      let n = m.split('').reduce(function (acc, b) {
         return acc + map[b];
       }, 0);
       return getInstruction(n, '-', '+');
     })
     .replace(regex.pointer, function (m) {
-      var map = { '>': 1, '<': -1 };
-      var n = m.split('').reduce(function (acc, b) {
+      let map = { '>': 1, '<': -1 };
+      let n = m.split('').reduce(function (acc, b) {
         return acc + map[b];
       }, 0);
       return getInstruction(n, '<', '>');
     })
   ;
   
-  // var ordersMap = { // m,p,o,i,l
+  // let ordersMap = { // m,p,o,i,l
   //   ',': 'm[p]=i();',
   //   '.': 'o(m[p]);',
   //   '[': 'while(m[p]){',
@@ -60,7 +60,7 @@ module.exports.compile = function (bfSource, userConfig) {
   //   '+': '++m[p];',
   //   '-': '--m[p];'
   // };
-  var ordersMap = { // m,p,o,i,l
+  let ordersMap = { // m,p,o,i,l
     ',': function () { return 'm[p]=i();'; },
     '.': function () { return 'o(m[p]);'; },
     '[': function () { return 'while(m[p]){'; },
@@ -70,47 +70,47 @@ module.exports.compile = function (bfSource, userConfig) {
     '+': function (count) { return 'm[p]+='+count+';'; },
     '-': function (count) { return 'm[p]-='+count+';'; }
   };
-  var createOrder = function (order, count) {
-    var prefix = actualConfig.maxInstructions > 0 ? [
+  let createOrder = function (order, count) {
+    let prefix = actualConfig.maxInstructions > 0 ? [
       'if(!--c)return;'
     ].join('') : '';
     return [prefix, ordersMap[order](count)].join('');
   };
-  var definitions = {
+  let definitions = {
     // count
     c: function (config) {
-      return config.maxInstructions > 0 ? 'var c='+config.maxInstructions+';' : '';
+      return config.maxInstructions > 0 ? 'let c='+config.maxInstructions+';' : '';
     },
     // length
     l: function (config) {
-      return ['var l=',config.memorySize,';'].join('');
+      return ['let l=',config.memorySize,';'].join('');
     },
     // memory
     m: function (config) {
-      var memConstructors = {
+      let memConstructors = {
         '8': 'Uint8Array',
         '16': 'Uint16Array',
         '32': 'Uint32Array'
       };
-      var constructorName = memConstructors[config.bits] || memConstructors[8];
-      return ['var m=new ',constructorName,'(l);'].join('');
+      let constructorName = memConstructors[config.bits] || memConstructors[8];
+      return ['let m=new ',constructorName,'(l);'].join('');
     },
     // pointer
     p: function () {
-      return 'var p=0;';
+      return 'let p=0;';
     },
     // out
     o: function () {
-      return 'var o=output||function(){};';
+      return 'let o=output||function(){};';
     },
     // in
     i: function () {
-      return 'var i=input||function(){return 0;};';
+      return 'let i=input||function(){return 0;};';
     }
   };
-  var arguments = ['input', 'output'];
+  let arguments = ['input', 'output'];
 
-  var code = [];
+  let code = [];
   _.each(definitions, function (evaluate) {
     code.push(evaluate(actualConfig));
   });
@@ -120,20 +120,20 @@ module.exports.compile = function (bfSource, userConfig) {
   // });
 
   (optimized.match(regex.instruction) || []).map(function (instruction) {
-    var count = +instruction.substr(0, instruction.length - 1) || 1;
-    var order = instruction.substr(-1);
+    let count = +instruction.substr(0, instruction.length - 1) || 1;
+    let order = instruction.substr(-1);
     code.push(createOrder(order, count));
   });
 
-  var compiled = new Function(arguments, code.join(''));
+  let compiled = new Function(arguments, code.join(''));
   return {
     run: function (input, output) {
-      var inp, out;
-      var res = [];
+      let inp, out;
+      let res = [];
       if (_.isString(input)) {
         input = input.split('');
         inp = function () {
-          var ch = input.shift();
+          let ch = input.shift();
           return ch ? ch.charCodeAt(0) : 0;
         };
       } else if (_.isFunction(input)) {
@@ -143,7 +143,7 @@ module.exports.compile = function (bfSource, userConfig) {
         output = function () {};
       }
       out = function (num) {
-        var ch = String.fromCharCode(num);
+        let ch = String.fromCharCode(num);
         output(num, ch);
         res.push(ch);
       };
