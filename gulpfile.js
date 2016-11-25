ce0e5b0324c3303839592db78c4ad90d8ae0af5d
Grzegorz Kućmierz <gkucmierz@gmail.com>
2016-11-25 20:37:20 +0100

    gulp test

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
+const watch = require('gulp-watch');
 const babel = require('gulp-babel');
 const modifyFile = require('gulp-modify-file');
+const cp = require('child_process');
  
 gulp.task('build', () => {
   gulp.src('brainfuck.js')
@@ -16,6 +18,14 @@ var brainfuckCompiler = (function (module){
       return `${start}${content}${end}`;
     }))
     .pipe(gulp.dest('lib'));
+});
+
+gulp.task('watch', () => {
+  return watch(['brainfuck.js', 'test.js'], () => {
+    cp.exec('npm test', {}, (err, res) => {
+      console.log(err ? err : res);
+    });
+  });
 });
 
 gulp.task('default', () => {
